74781404da931a6a3267b24a8d3f47b7ae6d7766
Sachin Walunjakar <walunjakarsachinedu@gmail.com>
2022-12-15 15:05:03 +0530

    add performance-trick file & refactored dp.txt

---
+++ addition algorithms/tricks/performance_trick.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "../../print.cpp"
+using namespace std;
+
+
+int main() {
+  // creating & intializing 2d vector // 10 * 10 matrix with default value -1
+  vector<vector<int>> matrix2d(10, vector<int>(10, -1));
+  // inserting multiple element in vector
+  vector<int> a; a.insert(a.end(), {1,2,3,4,5,6});
+  return 0;
+}
